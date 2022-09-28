a961e1219f70d669f74ce47c1c7f3928ce6e4546
Nafid Faham <nafidfaham08@gmail.com>
2022-09-28 18:44:14 +0600

    db: import mongoose db Es6 way

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,9 +1,7 @@
 import mongoose from 'mongoose'
 const MONGO_URI = process.env.MONGO_URI;
 
-const db = async () => {
+export const db = async () => {
   await mongoose.connect(MONGO_URI);
   console.log("conntected db");
 };
-
-module.exports = db;
